972b313ca834c42679c3b37468edf77d700b5822
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2023-11-27 14:48:21 -0500

    Basic sort

--- src/intervals/sort.rs
+++ src/intervals/sort.rs
@@ -1,14 +1,42 @@
+// TODO:
+// - testing.
+// - proper docs.
+
+use core::cmp::Ordering;
+
 use super::Interval;
 
-pub fn sort_by_start<T>(_v: &mut Vec<Interval<T>>)
+/// Complexity:
+/// O(n * log n) Runtime
+/// O(?) space
+/// Where n = ...
+///
+/// example...
+///
+// TODO: take an iterator, not a vec.
+pub fn sort_by_start<T>(v: &mut Vec<Interval<T>>)
 where
     T: Copy + Ord,
 {
-    todo!()
+    // consider: should sort_unstable_by be used instead?
+    v.sort_by(|a, b| {
+        let mut result = a.start.cmp(&b.start);
+        if let Ordering::Equal = result {
+            result = a.end.cmp(&b.end);
+        }
+        result
+    })
 }
-pub fn sort_by_end<T>(_v: &mut Vec<Interval<T>>)
+pub fn sort_by_end<T>(v: &mut Vec<Interval<T>>)
 where
     T: Copy + Ord,
 {
-    todo!()
+    // consider: should sort_unstable_by be used instead?
+    v.sort_by(|a, b| {
+        let mut result = a.end.cmp(&b.end);
+        if let Ordering::Equal = result {
+            result = a.end.cmp(&b.end);
+        }
+        result
+    })
 }
